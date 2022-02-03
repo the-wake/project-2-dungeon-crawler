6049939560f8ba9493ba81f18be269d996af9877
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-02 19:20:55 -0600

    added more routes to campaign-routes, dungeon-routes and room routes

--- controllers/api/room-routes.js
+++ controllers/api/room-routes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const { Room } = require('../../models');
 
+//get all rooms
 router.get('/', async (req, res) => {
-    // res.render('home-page');
-    res.status(200).json("room")
+    Room.findAll().then(roomData => {
+        const rooms = roomData.map((rms) => rms.get({ plain: true }));
+        res.render('room', { rooms });
+    })
 });
+
+//add a room
+router.post('/add-room', (req, res) => {
+    console.log(req.body);
+    Room.create(req.body).then(data => {
+        console.log('Room posted.')
+        res.redirect('/api/room');
+    })
+})
+
+
 module.exports = router;
