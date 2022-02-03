e16fb041cea8d0c82232c331fa57873385761058
Ben Martin <bmartin2009@gmail.com>
2022-02-03 17:26:42 -0600

    Added withAuth function.

--- controllers/api/doodad-routes.js
+++ controllers/api/doodad-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const withAuth = require('../../utils/auth.js')
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     // res.render('home-page');
     res.status(200).json("doodad")
 });
