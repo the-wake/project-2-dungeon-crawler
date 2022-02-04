aa50eb05b08ae1e8d8442829b17c3d3042cf2596
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-04 15:40:14 -0600

    changed forms appearance and changed button colors

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,14 +16,14 @@ router.get('/login', (req, res) => {
   // Otherwise, render the 'login' template
   res.render('login', { loggedIn: req.session.loggedIn });
 });
-// router.get('/newuser', (req, res) => {
-//   // If the user is already logged in, redirect to the homepage
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-//   // Otherwise, render the 'login' template
-//   res.render('sign-up');
-// });
+router.get('/newuser', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render('sign-up');
+});
 
 module.exports = router;
