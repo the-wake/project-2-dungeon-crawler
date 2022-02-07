03c5399962b3694f9d36d475f8535aab31d3d0a2
Ben Martin <bmartin2009@gmail.com>
2022-02-07 13:29:54 -0600

    Fixed creature creating

--- controllers/api/creature-routes.js
+++ controllers/api/creature-routes.js
@@ -4,8 +4,18 @@ const withAuth = require('../../utils/auth.js')
 
 // Creates a new creature
 router.post('/', withAuth, (req, res) => {
-    Creature.create(req.body).then(data => {
-        res.status(200).redirect('/creatures/new');
+    const creatureData = {...req.body}
+    if (creatureData.in_room === "") {
+        creatureData.in_room = null
+    }
+    Creature.create(creatureData).then(data => {
+        console.log(data);
+        if (creatureData.in_room === null) {
+            console.log(creatureData);
+            res.status(200).redirect(`/creatures/${data.id}`);
+            return;
+        }
+        res.status(200).redirect(`/rooms/${data.in_room}`);
     })
 });
 
